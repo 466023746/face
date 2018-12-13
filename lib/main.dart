9c350414002824a14059b8eb99f0cfa9644550dc
娄涛 <loutao@corp.netease.com>
2018-12-13 22:13:58 +0800

    feat: 更新流程图

--- lib/main.dart
+++ lib/main.dart
@@ -1,64 +1,3 @@
-//import 'package:flutter/material.dart';
-//
-//void main() {
-//  runApp(App());
-//}
-//
-//class App extends StatelessWidget {
-//  @override
-//  Widget build(BuildContext context) {
-//    return MaterialApp(title: 'Face', home: Home());
-//  }
-//}
-//
-//class Home extends StatelessWidget {
-//  @override
-//  Widget build(BuildContext context) {
-//    return Scaffold(
-//        appBar: AppBar(
-//          title: Text('Face'),
-//        ),
-//        body: Center(
-//          child: Column(
-//            mainAxisAlignment: MainAxisAlignment.center,
-//            children: <Widget>[
-//              Text(
-//                '这是一个头像识别游戏',
-//                style: TextStyle(fontSize: 18.0),
-//              ),
-//              HomeButton()
-//            ],
-//          ),
-//        ));
-//  }
-//}
-//
-//class HomeButton extends StatelessWidget {
-//  @override
-//  Widget build(BuildContext context) {
-//    return GestureDetector(
-//      onTap: () {
-//        print('MyButton was tapped!');
-//      },
-//      child: Container(
-//        height: 46.0,
-//        padding: const EdgeInsets.all(8.0),
-//        margin: const EdgeInsets.symmetric(horizontal: 8.0, vertical: 8.0),
-//        decoration: BoxDecoration(
-//          borderRadius: BorderRadius.circular(5.0),
-//          color: Colors.blue,
-//        ),
-//        child: Center(
-//          child: Text(
-//            '玩玩看',
-//            style: TextStyle(fontSize: 16.0, color: Colors.white),
-//          ),
-//        ),
-//      ),
-//    );
-//  }
-//}
-
 import 'dart:async';
 import 'dart:io';
 
@@ -69,6 +8,45 @@ import 'package:video_player/video_player.dart';
 import 'package:dio/dio.dart';
 import 'package:flutter/services.dart' show rootBundle;
 
+List<CameraDescription> cameras;
+Dio dio;
+List refPics;
+String imagePath;
+final double padding = 15.0;
+
+Future<void> main() async {
+  // Fetch the available cameras before initializing the app.
+  try {
+    cameras = await availableCameras();
+  } on CameraException catch (e) {
+    logError(e.code, e.description);
+  }
+  dio = getHttpClient();
+  refPics = getRefPics();
+  runApp(CameraApp());
+}
+
+Dio getHttpClient() {
+  var dio = new Dio();
+
+  return dio;
+}
+
+List getRefPics() {
+  List refPics = ['images/me.jpeg'];
+
+  return refPics;
+}
+
+class CameraApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: CameraExampleHome(),
+    );
+  }
+}
+
 class CameraExampleHome extends StatefulWidget {
   @override
   _CameraExampleHomeState createState() {
@@ -76,127 +54,141 @@ class CameraExampleHome extends StatefulWidget {
   }
 }
 
-/// Returns a suitable camera icon for [direction].
-IconData getCameraLensIcon(CameraLensDirection direction) {
-  switch (direction) {
-    case CameraLensDirection.back:
-      return Icons.camera_rear;
-    case CameraLensDirection.front:
-      return Icons.camera_front;
-    case CameraLensDirection.external:
-      return Icons.camera;
+class _CameraExampleHomeState extends State<CameraExampleHome> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: appBar(needBack: false),
+      body: ListView(
+        children: <Widget>[
+          Container(
+              margin: EdgeInsets.only(top: 30.0, bottom: 10.0),
+              padding: EdgeInsets.symmetric(horizontal: padding),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  Text('人脸识别小游戏', style: TextStyle(fontSize: 20.0))
+                ],
+              )),
+          _titleWidget('玩法'),
+          _textWidget('点击开始按钮进行自拍，自拍完成后系统与图库进行比对，然后寻找与你最接近的人脸。'),
+          _titleWidget('注意'),
+          _textWidget('系统会自动存储你的自拍图像，该图像只用于游戏内，不会在任何其他地方使用和展示。')
+        ],
+      ),
+      floatingActionButton: FloatingActionButton(
+          child: Text('开始'),
+          onPressed: () {
+            Navigator.push(
+              context,
+              // todo
+              MaterialPageRoute(builder: (context) => FaceInfo()),
+            );
+          }),
+    );
   }
-  throw ArgumentError('Unknown lens direction');
+
+  Widget _titleWidget(title) {
+    return Container(
+        margin: EdgeInsets.only(top: 20.0),
+        padding: EdgeInsets.symmetric(horizontal: padding),
+        child: Text(
+          title,
+          style: TextStyle(fontSize: 18.0, fontWeight: FontWeight.bold),
+        ));
+  }
+
+  Widget _textWidget(text) {
+    return Container(
+        margin: EdgeInsets.only(top: 10.0),
+        padding: EdgeInsets.symmetric(horizontal: padding),
+        child: Text(text, style: TextStyle(fontSize: 16.0), softWrap: true));
+  }
 }
 
-void logError(String code, String message) =>
-    print('Error: $code\nError Message: $message');
+Widget appBar({String title = 'Face', bool needBack = true}) {
+  return AppBar(
+    title: Text(title),
+    leading: Builder(
+      builder: (BuildContext context) {
+        if (needBack) {
+          return IconButton(
+            icon: const Icon(Icons.arrow_back_ios),
+            onPressed: () {
+              Navigator.pop(context);
+            },
+          );
+        }
+        return Container(
+          width: 0,
+          height: 0,
+        );
+      },
+    ),
+  );
+}
 
-class _CameraExampleHomeState extends State<CameraExampleHome> {
+class FaceCamera extends StatefulWidget {
+  @override
+  _FaceCameraState createState() {
+    return _FaceCameraState();
+  }
+}
+
+class _FaceCameraState extends State<FaceCamera> {
   CameraController controller;
   String videoPath;
   VideoPlayerController videoController;
   VoidCallback videoPlayerListener;
-  int contentShow = 1;
-  var homeContext;
+  BuildContext globalContext;
+
+  // 摄像头前后置index
+  int descriptionIndex;
 
   final GlobalKey<ScaffoldState> _scaffoldKey = GlobalKey<ScaffoldState>();
 
   @override
+  void initState() {
+    super.initState();
+    descriptionIndex = 0;
+  }
+
+  @override
   Widget build(BuildContext context) {
-    homeContext = context;
+    globalContext = context;
 
     return Scaffold(
         key: _scaffoldKey,
-        appBar: AppBar(
-          title: const Text('Face'),
-        ),
-        body: _bodyWidget());
-  }
-
-  Widget _bodyWidget() {
-    if (contentShow == 1) {
-      return AlertDialog(
-        title: Text('提示'),
-        content: Text('这是一个头像识别游戏，你可以点击下面的按钮来一张自拍体验一下'),
-        actions: <Widget>[
-          FlatButton(
-            child: Text('好的'),
-            onPressed: () {
-              setState(() {
-                contentShow = 2;
-              });
-            },
-          ),
-          FlatButton(
-            child: Text('不要'),
-            onPressed: () {
-              setState(() {
-                contentShow = 3;
-              });
-            },
-          )
-        ],
-      );
-    } else if (contentShow == 2) {
-      return Column(children: <Widget>[
-        Expanded(
-          child: Container(
-            child: Padding(
-              padding: const EdgeInsets.all(1.0),
-              child: Center(
-                child: _cameraPreviewWidget(),
-              ),
-            ),
-            decoration: BoxDecoration(
-              color: Colors.black,
-              border: Border.all(
-                color: controller != null && controller.value.isRecordingVideo
-                    ? Colors.redAccent
-                    : Colors.grey,
-                width: 3.0,
-              ),
-            ),
-          ),
-        ),
-//          _captureControlRowWidget(),
-        Padding(
-            padding: const EdgeInsets.all(5.0),
-            child: _cameraTogglesRowWidget()),
-      ]);
-    } else {
-      return Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: <Widget>[
-          Center(
-            child: Text('确定不要试一试吗？', style: TextStyle(fontSize: 16.0)),
-          ),
-          Row(
-            children: <Widget>[
-              Expanded(
-                  child: Container(
-                padding: EdgeInsets.symmetric(horizontal: 8.0),
-                child: RaisedButton(
-                  child: Text('试一试'),
-                  onPressed: () {
-                    setState(() {
-                      contentShow = 2;
-                    });
-                  },
+        appBar: appBar(),
+        body: Column(children: <Widget>[
+          Expanded(
+            child: Container(
+              child: Padding(
+                padding: const EdgeInsets.all(1.0),
+                child: Center(
+                  child: _cameraPreviewWidget(),
                 ),
-              ))
-            ],
-          )
-        ],
-      );
-    }
+              ),
+              decoration: BoxDecoration(
+                color: Colors.black,
+                border: Border.all(
+                  color: Colors.grey,
+                  width: 3.0,
+                ),
+              ),
+            ),
+          ),
+          Padding(
+              padding: const EdgeInsets.all(5.0),
+              child: _cameraTogglesRowWidget()),
+        ]));
   }
 
   /// Display the preview from the camera (or a message if the preview is not available).
   Widget _cameraPreviewWidget() {
     if (controller == null || !controller.value.isInitialized) {
       return const Text(
-        'Tap a camera',
+        '初始化...',
         style: TextStyle(
           color: Colors.white,
           fontSize: 24.0,
@@ -211,77 +203,41 @@ class _CameraExampleHomeState extends State<CameraExampleHome> {
     }
   }
 
-  /// Display the thumbnail of the captured image or video.
-//  Widget _thumbnailWidget() {
-//    return Expanded(
-//      child: Align(
-//        alignment: Alignment.centerRight,
-//        child: videoController == null && imagePath == null
-//            ? null
-//            : SizedBox(
-//                child: (videoController == null)
-//                    ? Image.file(File(imagePath))
-//                    : Container(
-//                        child: Center(
-//                          child: AspectRatio(
-//                              aspectRatio: videoController.value.size != null
-//                                  ? videoController.value.aspectRatio
-//                                  : 1.0,
-//                              child: VideoPlayer(videoController)),
-//                        ),
-//                        decoration: BoxDecoration(
-//                            border: Border.all(color: Colors.pink)),
-//                      ),
-//                width: 64.0,
-//                height: 64.0,
-//              ),
-//      ),
-//    );
-//  }
-
-  /// Display the control bar with buttons to take pictures and record videos.
-//  Widget _captureControlRowWidget() {
-//    return IconButton(
-//      icon: const Icon(Icons.camera_alt),
-//      color: Colors.blue,
-//      onPressed: controller != null &&
-//              controller.value.isInitialized &&
-//              !controller.value.isRecordingVideo
-//          ? onTakePictureButtonPressed
-//          : null,
-//    );
-//  }
-
   /// Display a row of toggle to select the camera (or a message if no camera is available).
   Widget _cameraTogglesRowWidget() {
     final List<Widget> toggles = <Widget>[];
 
     if (cameras.isEmpty) {
-      toggles.add(Text('No camera found'));
+      toggles.add(Text(
+        '没有找到摄像头',
+        style: TextStyle(fontSize: 20.0),
+      ));
     } else {
-      for (CameraDescription cameraDescription in cameras) {
-        toggles.add(SizedBox(
+      // 切换前后置按钮
+      toggles.add(SizedBox(
           width: 90.0,
-          child: RadioListTile<CameraDescription>(
-            title: Icon(getCameraLensIcon(cameraDescription.lensDirection)),
-            groupValue: controller?.description,
-            value: cameraDescription,
-            onChanged: controller != null && controller.value.isRecordingVideo
-                ? null
-                : onNewCameraSelected,
-          ),
-        ));
-      }
+          child: IconButton(
+              iconSize: 30.0,
+              color: Colors.grey,
+              icon: Icon(Icons.switch_camera),
+              onPressed: () {
+                if (controller != null) {
+                  onNewCameraSelected(cameras[descriptionIndex]);
+                  descriptionIndex++;
+                  if (descriptionIndex == cameras.length) {
+                    descriptionIndex = 0;
+                  }
+                }
+              })));
 
+      // 拍照按钮
       toggles.add(
         SizedBox(
             width: 90.0,
             child: IconButton(
-              icon: const Icon(Icons.camera_alt),
-              color: Colors.blue,
-              onPressed: controller != null &&
-                      controller.value.isInitialized &&
-                      !controller.value.isRecordingVideo
+              iconSize: 30.0,
+              icon: Icon(Icons.camera_alt),
+              onPressed: controller != null && controller.value.isInitialized
                   ? onTakePictureButtonPressed
                   : null,
             )),
@@ -292,12 +248,6 @@ class _CameraExampleHomeState extends State<CameraExampleHome> {
       mainAxisAlignment: MainAxisAlignment.center,
       children: toggles,
     );
-  }
-
-  String timestamp() => DateTime.now().millisecondsSinceEpoch.toString();
-
-  void showInSnackBar(String message) {
-    _scaffoldKey.currentState.showSnackBar(SnackBar(content: Text(message)));
   }
 
   void onNewCameraSelected(CameraDescription cameraDescription) async {
@@ -325,99 +275,17 @@ class _CameraExampleHomeState extends State<CameraExampleHome> {
     }
   }
 
+  void showInSnackBar(String message) {
+    _scaffoldKey.currentState.showSnackBar(SnackBar(content: Text(message)));
+  }
+
   void onTakePictureButtonPressed() {
     takePicture().then((String filePath) {
-      if (mounted) {
-        setState(() {
-          imagePath = filePath;
-          videoController?.dispose();
-          videoController = null;
-        });
-        if (filePath != null) showInSnackBar('Picture saved to $filePath');
-        Navigator.push(
-          homeContext,
-          MaterialPageRoute(builder: (context) => FaceInfo()),
-        );
-      }
+      Navigator.push(
+        globalContext,
+        MaterialPageRoute(builder: (context) => FaceInfo()),
+      );
     });
-  }
-
-  void onVideoRecordButtonPressed() {
-    startVideoRecording().then((String filePath) {
-      if (mounted) setState(() {});
-      if (filePath != null) showInSnackBar('Saving video to $filePath');
-    });
-  }
-
-  void onStopButtonPressed() {
-    stopVideoRecording().then((_) {
-      if (mounted) setState(() {});
-      showInSnackBar('Video recorded to: $videoPath');
-    });
-  }
-
-  Future<String> startVideoRecording() async {
-    if (!controller.value.isInitialized) {
-      showInSnackBar('Error: select a camera first.');
-      return null;
-    }
-
-    final Directory extDir = await getApplicationDocumentsDirectory();
-    final String dirPath = '${extDir.path}/Movies/flutter_test';
-    await Directory(dirPath).create(recursive: true);
-    final String filePath = '$dirPath/${timestamp()}.mp4';
-
-    if (controller.value.isRecordingVideo) {
-      // A recording is already started, do nothing.
-      return null;
-    }
-
-    try {
-      videoPath = filePath;
-      await controller.startVideoRecording(filePath);
-    } on CameraException catch (e) {
-      _showCameraException(e);
-      return null;
-    }
-    return filePath;
-  }
-
-  Future<void> stopVideoRecording() async {
-    if (!controller.value.isRecordingVideo) {
-      return null;
-    }
-
-    try {
-      await controller.stopVideoRecording();
-    } on CameraException catch (e) {
-      _showCameraException(e);
-      return null;
-    }
-
-    await _startVideoPlayer();
-  }
-
-  Future<void> _startVideoPlayer() async {
-    final VideoPlayerController vcontroller =
-        VideoPlayerController.file(File(videoPath));
-    videoPlayerListener = () {
-      if (videoController != null && videoController.value.size != null) {
-        // Refreshing the state to update video player with the correct ratio.
-        if (mounted) setState(() {});
-        videoController.removeListener(videoPlayerListener);
-      }
-    };
-    vcontroller.addListener(videoPlayerListener);
-    await vcontroller.setLooping(true);
-    await vcontroller.initialize();
-    await videoController?.dispose();
-    if (mounted) {
-      setState(() {
-        imagePath = null;
-        videoController = vcontroller;
-      });
-    }
-    await vcontroller.play();
   }
 
   Future<String> takePicture() async {
@@ -444,46 +312,42 @@ class _CameraExampleHomeState extends State<CameraExampleHome> {
     return filePath;
   }
 
+  String timestamp() => DateTime.now().millisecondsSinceEpoch.toString();
+
   void _showCameraException(CameraException e) {
     logError(e.code, e.description);
     showInSnackBar('Error: ${e.code}\n${e.description}');
   }
 }
 
-class CameraApp extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      home: CameraExampleHome(),
-    );
-  }
-}
-
-List<CameraDescription> cameras;
-var dio;
-var refPics;
-String imagePath;
-
-getHttpClient() {
-  var dio = new Dio();
-
-  return dio;
-}
-
-List getRefPics() {
-  List refPics = ['images/me.jpeg'];
-
-  return refPics;
-}
-
 class FaceInfo extends StatefulWidget {
   @override
   _FaceInfoState createState() {
-    _compare();
     return _FaceInfoState();
   }
+}
 
-  Future _compare() async {
+class _FaceInfoState extends State<FaceInfo> {
+  var response;
+
+  @override
+  void initState() {
+    super.initState();
+    // todo
+    response = {
+      "time_used": 473,
+      "confidence": 96.46,
+      "thresholds": {
+        "1e-3": 65.3,
+        "1e-5": 76.5,
+        "1e-4": 71.8
+      },
+      "request_id": "1469761507,07174361-027c-46e1-811f-ba0909760b18"
+    };
+//    _compare();
+  }
+
+  Future<void> _compare() async {
     final apiKey = '4OBMLbwJjOGx6zdRSfE64sCyXUtPOYYn';
     final apiSecret = 'TbZUyEmYbYX0lFV8LQ9AAnMDXM6wX0dl';
     var pic = await rootBundle.load(refPics[0]);
@@ -501,25 +365,11 @@ class FaceInfo extends StatefulWidget {
     var response = await dio.post(url, data: formData);
     print(response);
   }
-}
 
-class _FaceInfoState extends State<FaceInfo> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Face Info'),
-        leading: Builder(
-          builder: (BuildContext context) {
-            return IconButton(
-              icon: const Icon(Icons.menu),
-              onPressed: () {
-                Navigator.pop(context);
-              },
-            );
-          },
-        ),
-      ),
+      appBar: appBar(),
       body: Column(
         children: <Widget>[
           SizedBox(
@@ -535,14 +385,5 @@ class _FaceInfoState extends State<FaceInfo> {
   }
 }
 
-Future<void> main() async {
-  // Fetch the available cameras before initializing the app.
-  try {
-    cameras = await availableCameras();
-  } on CameraException catch (e) {
-    logError(e.code, e.description);
-  }
-  dio = getHttpClient();
-  refPics = getRefPics();
-  runApp(CameraApp());
-}
+void logError(String code, String message) =>
+    print('Error: $code\nError Message: $message');
